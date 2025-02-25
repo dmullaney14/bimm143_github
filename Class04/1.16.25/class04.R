bdd8f9d5ca4105696e739a20a7355b04fb0fd5e4
Dylan Mullaney <dmullaney@ucsd.edu>
2025-02-25 11:50:48 -0800

    renaming

---
+++ Class04/1.16.25/class04.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: Dylan (A16869792)
+#' ---
+
+
+x <- 1:50
+plot(x, sin(x))
+
+# Make more sense of the graph
+plot(x, sin(x), col='blue', type="l", lwd=3)
